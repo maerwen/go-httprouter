6605741593e8b28e6848e04b723fda06e262caf0
wangzhaoxiang <wangzhaoxiang@huania.com>
2018-06-28 15:06:59 +0800

    tree

--- src/tree.go
+++ src/tree.go
@@ -1,6 +1,18 @@
 package httprouter
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+// 常量
+const (
+	static   nodeType = iota //default
+	root                     //根节点
+	param                    //参数匹配
+	catchAll                 //全匹配
+)
 
 //nodeType 类型
 type nodeType uint8
@@ -273,8 +285,314 @@ func (n *node) insertChild(numParams uint8, path, fullPath string, handle Handle
 // 通配符的值被存储到了一个map中
 // 如果该路径没有对应的handle,但却有一个在其基础上尾部含有'/'的路径,建议重定向
 func (n *node) getValue(path string) (handle Handle, p Params, tsr bool) {
-
+walk:
+	for {
+		if len(path) > len(n.path) {
+			if path[:len(n.path)] == n.path {
+				path = path[len(n.path):]
+				// 如果当前节点没有一个通配符,我们只能继续检索词典树和下一个子节点
+				if !n.wildChild {
+					c := path[0]
+					for i := 0; i < len(n.indices); i++ {
+						if c == n.indices[i] {
+							n = n.children[i]
+							continue walk
+						}
+					}
+					// 没有找到,
+					// 如果一个同网址的链条存在,我们可以建议重定向到相同的不包含'/'的网址
+					tsr = (path == "/" && n.handle != nil)
+					return
+				}
+				// 处理通配符子节点
+				n = n.children[0]
+				switch n.nType {
+				case param:
+					// 发现param结束('/'或结束)
+					end := 0
+					for end < len(path) && path[end] != '/' {
+						end++
+					}
+					// 保存param的value
+					if p == nil {
+						// 延迟分配
+						p = make(Params, 0, n.maxParams)
+					}
+					i := len(p)
+					// 在预先分配的容量内展开切片
+					p = p[:i+1]
+					p[i].Key = n.path[1:]
+					p[i].Value = path[:end]
+					// 进一步深入
+					if end < len(path) {
+						if len(n.children) > 0 {
+							path = path[end:]
+							n = n.children[0]
+							continue walk
+						}
+						//
+						tsr = (len(path) == end+1)
+						return
+					}
+					if handle = n.handle; handle != nil {
+						return
+					} else if len(n.children) == 1 {
+						// 没有处理器,检查是否存在一个处理该路径上带'/'的处理器
+						n = n.children[0]
+						tsr = (n.path == "/" && n.handle != nil)
+					}
+					return
+				case catchAll:
+					// 保存param的value
+					if p == nil {
+						// 延迟分配
+						p = make(Params, 0, n.maxParams)
+					}
+					i := len(p)
+					// 在预先分配的容量内展开切片
+					p = p[:i+1]
+					p[i].Key = n.path[2:]
+					p[i].Value = path
+					handle = n.handle
+					return
+				default:
+					panic("invalid node type")
+				}
+			}
+		} else if path == n.path {
+			// 或许我们又来到了已经包含handle处理器的节点
+			// 检查我们所找的节点是否已经有处理器
+			if handle = n.handle; handle != nil {
+				return
+			}
+			if path == "/" && n.wildChild && n.nType != root {
+				tsr = true
+				return
+			}
+			// 没有处理器,检查是否存在一个处理该路径上带'/'的处理器
+			for i := 0; i < len(n.indices); i++ {
+				if n.indices[i] == '/' {
+					n = n.children[i]
+					tsr = (len(n.path) == 1 && n.handle != nil) ||
+						(n.nType == catchAll && n.children[0].handle != nil)
+					return
+				}
+			}
+			return
+		}
+		// 没有找到,
+		// 如果一个同网址的链条存在,我们可以建议重定向到相同的不包含'/'的网址
+		tsr = (path == "/") ||
+			(len(n.path) == len(path)+1 && n.path[len(path)] == '/' &&
+				path == n.path[:len(n.path)-1] && n.handle != nil)
+		return
+	}
 }
 
-// findCaseInsensitivePath方法，对大小写不敏感的path区分对待
-// findCaseInsensitivePathRec递归
+// findCaseInsensitivePath方法
+// 利用给定的路径进行一次大小写敏感的检索并尝试去找到一个处理器
+// 它可以随意的修正尾部的'/'
+// 返回一个大小写校正的路径和一个表明检索是否成功的布尔值
+func (n *node) findCaseInsensitivePath(path string, fixTrailingSlash bool) (ciPath []byte, found bool) {
+	return n.findCaseInsensitivePathRec(
+		path,
+		strings.ToLower(path),
+		// 为新路径预留足够内存
+		make([]byte, 0, len(path)+1),
+		// 空字符缓冲区
+		[4]byte{},
+		fixTrailingSlash,
+	)
+}
+
+// findCaseInsensitivePathRec
+// 递归大小写敏感的检索功能
+func (n *node) findCaseInsensitivePathRec(path, loPath string, ciPath []byte, rb [4]byte, fixTrailingSlash bool) ([]byte, bool) {
+	loNPath := strings.ToLower(n.path)
+walk: //遍历词典树的外层循环
+	for len(loPath) >= len(loNPath) && (len(loNPath) == 0 || loPath[1:len(loNPath)] == loNPath[1:]) {
+		// 给结果添加公共路径
+		ciPath = append(ciPath, n.path...)
+		if path = path[len(n.path):]; len(path) > 0 {
+			loOld := loPath
+			loPath = loPath[len(loNPath):]
+			// 如果当前节点没有一个通配符子节点,我们可以检索下一个子节点并且继续遍历词典树
+			if !n.wildChild {
+				//跳过已经处理过的字节数组
+				rb = shiftNRuneBytes(rb, len(loNPath))
+				if rb[0] != 0 {
+					// 旧的字节还未结束
+					for i := 0; i < len(n.indices); i++ {
+						if n.indices[i] == rb[0] {
+							// 继续对子节点进行该操作
+							n = n.children[i]
+							loNPath = strings.ToLower(n.path)
+							continue walk
+						}
+					}
+				} else {
+					// 处理新的字节数据
+					var rv rune
+					// 查找开始数据
+					// 数据长度为4
+					var off int
+					for max := min(len(loNPath), 3); off < max; off++ {
+						if i := len(loNPath) - off; utf8.RuneStart(loOld[i]) {
+							//从缓存的小写路径中读取数据
+							rv, _ = utf8.DecodeRuneInString(loOld[i:])
+							break
+						}
+					}
+					//计算当前数据的小写字节
+					utf8.EncodeRune(rb[:], rv)
+					// 跳过已处理的数据
+					rb = shiftNRuneBytes(rb, off)
+					for i := 0; i < len(n.indices); i++ {
+						// 小写匹配
+						if n.indices[i] == rb[0] {
+							// 必须使用递归去处理,直到大写和小写都作为索引而存在
+							if out, found := n.children[i].findCaseInsensitivePathRec(
+								path, loPath, ciPath, rb, fixTrailingSlash,
+							); found {
+								return out, true
+							}
+							break
+						}
+					}
+					//如果不同,对于大写字符也是如此
+					if up := unicode.ToUpper(rv); up != rv {
+						utf8.EncodeRune(rb[:], up)
+						rb = shiftNRuneBytes(rb, off)
+						for i := 0; i < len(n.indices); i++ {
+							// 大写匹配
+							if n.indices[i] == rb[0] {
+								// 继续对子节点进行处理
+								n = n.children[i]
+								loNPath = strings.ToLower(n.path)
+								continue walk
+							}
+						}
+					}
+				}
+				// 没有找到,
+				// 如果一个同网址的链条存在,我们可以建议重定向到相同的不包含'/'的网址
+				return ciPath, (fixTrailingSlash && path == "/" && n.handle != nil)
+			}
+			n = n.children[0]
+			switch n.nType {
+			case param:
+				// 发现param结束('/'或者结束)
+				k := 0
+				for k < len(path) && path[k] != '/' {
+					k++
+				}
+				// 把param的值添加到大小写不敏感的path上去
+				ciPath = append(ciPath, path[:k]...)
+				// 进一步深入
+				if k < len(path) {
+					if len(n.children) > 0 {
+						// 在子节点上继续
+						n = n.children[0]
+						loNPath = strings.ToLower(n.path)
+						loPath = loPath[k:]
+						path = path[k:]
+						continue
+					}
+					//
+					if fixTrailingSlash && len(path) == k+1 {
+						return ciPath, false
+					}
+					return ciPath, false
+				}
+				if n.handle != nil {
+					return ciPath, true
+				} else if fixTrailingSlash && len(n.children) == 1 {
+					// 没有处理器,检查是否存在一个处理该路径上带'/'的处理器
+					n = n.children[0]
+					if n.path == "/" && n.handle != nil {
+						return append(ciPath, '/'), true
+					}
+				}
+				return ciPath, false
+			case catchAll:
+				return append(ciPath, path...), true
+			default:
+				panic("invalid node byte")
+			}
+		} else {
+			// 或许我们又来到了已经包含handle处理器的节点
+			// 检查我们所找的节点是否已经有处理器
+			if n.handle != nil {
+				return ciPath, true
+			}
+			// 如果没发现handle,就尝试去通过添加一个'/'去修正路径
+			if fixTrailingSlash {
+				for i := 0; i < len(n.indices); i++ {
+					if n.indices[i] == '/' {
+						n = n.children[i]
+						if (len(n.path) == 1 && n.handle != nil) ||
+							(n.nType == catchAll && n.children[0].handle != nil) {
+							return append(ciPath, '/'), true
+						}
+						return ciPath, false
+					}
+				}
+			}
+			return ciPath, false
+		}
+	}
+	// 如果没找到,就尝试通过添加/移除一个'/'去修正路径
+	if fixTrailingSlash {
+		if path == "/" {
+			return ciPath, true
+		}
+		if len(loPath)+1 == len(loNPath) && loNPath[len(loPath)] == '/' &&
+			loPath[1:] == loNPath[1:len(loPath)] && n.handle != nil {
+			return append(ciPath, n.path...), true
+		}
+	}
+	return ciPath, false
+}
+
+// 公用函数
+// min 返回两个数之间较小值
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+// countParams，计算params的个数
+func countParams(path string) uint8 {
+	var n uint
+	for i := 0; i < len(path); i++ {
+		// 与两种路径匹配关键字符比较
+		if path[i] != ':' && path[i] != '*' {
+			continue
+		}
+		n++
+	}
+	// url上限255byte
+	if n >= 255 {
+		return 255
+	}
+	return uint8(n)
+}
+
+// shiftNRuneBytes将给定数组中元素按照给定的数字向左移位
+// shift bytes in array by n bytes left
+func shiftNRuneBytes(rb [4]byte, n int) [4]byte {
+	switch n {
+	case 0:
+		return rb
+	case 1:
+		return [4]byte{rb[1], rb[2], rb[3], 0}
+	case 2:
+		return [4]byte{rb[2], rb[3]}
+	case 3:
+		return [4]byte{rb[3]}
+	default:
+		return [4]byte{}
+	}
+}
